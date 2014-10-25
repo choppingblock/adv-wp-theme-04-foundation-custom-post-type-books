b7f32fb864c75b72232b6618f15b5874c1831e18
choppingblock <matthew@choppingblock.com>
2014-10-25 02:01:08 -0400

    Initial check in of the theme we made in class.

---
+++ archive-cb_book.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+<div class="row">
+<!-- Row for main content area -->
+	<div class="small-12 large-8 columns" role="main">
+
+		<h1>archive-cb_book</h1>
+
+	<?php if ( have_posts() ) : ?>
+
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<a href="<?php echo get_post_permalink(); ?>" title="the book" >
+			<?php
+			if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+			  the_post_thumbnail();
+			}
+			?>
+			</a>
+		<?php endwhile; ?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+
+	<?php endif; // end have_posts() check ?>
+
+	<?php /* Display navigation to next/previous pages when applicable */ ?>
+	<?php if ( function_exists('FoundationPress_pagination') ) { FoundationPress_pagination(); } else if ( is_paged() ) { ?>
+		<nav id="post-nav">
+			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'FoundationPress' ) ); ?></div>
+			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'FoundationPress' ) ); ?></div>
+		</nav>
+	<?php } ?>
+
+	</div>
+	<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
